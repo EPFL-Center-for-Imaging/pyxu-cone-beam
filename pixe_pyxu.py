ffb37e32b4519d1eb74f8695c437192c2b977013
Youssef Haouchat <112254388+HaouchatY@users.noreply.github.com>
2023-12-08 21:48:41 +0100

    Update pixe_pyxu.py

--- pixe_pyxu.py
+++ pixe_pyxu.py
@@ -10,6 +10,8 @@ import pyxu.experimental.xray as pxr
 import pyxu.runtime as pxrt
 import pyxu.opt.stop as pxst
 
+from pyxu.operator import Gradient, SquaredL2Norm, L1Norm, PositiveL1Norm
+from pyxu.opt.solver import PGD
 
 
 def get_info(path: plib.Path):
@@ -53,39 +55,62 @@ def get_info(path: plib.Path):
 
     return sod, sdd, v_shape, v_pitch, P
 
+def setOperator(sod, sdd, v_shape, v_pitch, P, rx_pitch, dws, roi='False', shift_cor=0, center_slice='False'):
+    '''
+    Parameters
+    ----------
+    sod: float
+        Src-object distance.
+    sdd: float
+        Src-detector distance.
+    v_shape: tuple[int]
+        (x, y, z) pixel count.
+    v_pitch: tuple[float]
+        (x, y, z) pixel pitch.
+    P: np.ndarray[float32]
+        (N_view, N_h, N_w) projections.
+    rx_pitch: tuple[float]
+        (x, y) detector pitch.
+    dws: int
+        Down-sampling rate.
+    roi: int
+        Region of interest.
+    shift_cor: float
+        Shift correction value for center of rotation.
+    center_slice: bool
+        If True, only center slice of projections is used for reconstruction.
+    Returns
+    -------
+    op: pxr.XRayTransform
+        Operator.
+    P: np.ndarray[float32]
+        (N_view, N_h, N_w) projections.
+    v_shape: tuple[int]
+        (x, y, z) pixel count.
 
-if __name__ == "__main__":
-    # All coordinates are in XYZ ordering.
-    # Reference frame centered at base of rotating plate, with:
-    # * XZ-plane horizontal (touching bottom of detector);
-    # * Y-axis pointing up;
-    # * Z-axis pointing towards TX.
-
-    # A-priori knowledge of the datasets and machine. =========================
-    # dataset, dws = "000-HR", 2  # folder, down-sampling rate
-    dataset, dws = "bin2", 2
-    rx_pitch = (127e-3, 127e-3)  # 0.127 [mm] detector/receiver pitch (height, width)
+        
+    '''
     
-    # Load setup parameters from disk. ========================================
-    froot = plib.Path("~/Downloads/M2EA99-000").expanduser()
-    fpath = froot / dataset
-    sod, sdd, v_shape, v_pitch, P = get_info(fpath)
     #flip image of each view
-    
-    # ROI
-    #roi = 5
-    #P = P[:, P.shape[1]//2 - roi: P.shape[1]//2 + roi, :]
+    if roi != 'False':
+        P = P[:, P.shape[1]//2 - roi: P.shape[1]//2 + roi, :]
 
-    P = P[:, P.shape[1]//2, :]
-    N_view, N_w = P.shape
-    N_h = 1
-    v_shape = (v_shape[0], 1, v_shape[2])
+    N_view, N_h, N_w = P.shape
+
+    if center_slice == 'True':
+        P = P[:, P.shape[1]//2, :]
+        N_view, N_w = P.shape
+        N_h = 1
+        v_shape = (v_shape[0], 1, v_shape[2])
+
     # Compute TX position (1 projection). =====================================
     rx_h = rx_pitch[0] * dws * N_h  # detector height [mm]
     rx_w = rx_pitch[1] * dws * N_w  # detector width  [mm]
+
+    # normalize data    
+    P  = -np.log(P/P.max())
     
-    shift_cor = -0.3
-    tx_pos = np.r_[shift_cor, rx_h / 2, sod ]  # (3,)
+    tx_pos = np.r_[shift_cor, rx_h / 2, sod]  # (3,)
 
     # Compute RX position (1 projection). =====================================
     rx_ll = np.r_[-rx_w / 2 + shift_cor, 0, sod - sdd ]  # detector lower-left corner
@@ -134,95 +159,139 @@ if __name__ == "__main__":
         t_spec=t_spec,
     )
 
-    #fig = op.diagnostic_plot(slice(None, None, 600_000))
-    #fig.savefig("./diagnostic_plot.png", dpi=300)
+    return op, P, v_shape
 
+def reconstruct(fpath, dataset, dws, rx_pitch, method = 'pinv', roi='False', shift_cor=0., center_slice='False', max_iter=40, damp=0.1):
     '''
-    plt.figure('tilt series')
-    for i in range(P.shape[0]):
-        plt.imshow(P[i,:,:])
-        plt.pause(0.1)
-        plt.clf()
+    Parameters
+    ----------
+    fpath: plib.Path
+        Folder where .xml file and raw data is located.
+    dataset: str
+        Name of dataset.
+    dws: int
+        Down-sampling rate.
+    rx_pitch: tuple[float]
+        (x, y) detector pitch.
+    method: str
+        Reconstruction method : 'pinv', 'tv'.
+    roi: int
+        Region of interest.
+    shift_cor: float
+        Shift correction value for center of rotation.
+    center_slice: bool
+        If True, only center slice of projections is used for reconstruction.
+    max_iter: int
+        Maximum number of iterations.
+    damp: float
+        Damping factor.
+    Returns
+    -------
+    V_bw: np.ndarray[float32]
+        (x, y, z) reconstruction.
     '''
 
+    sod, sdd, v_shape, v_pitch, P = get_info(fpath)
+    op, P, v_shape = setOperator(sod, sdd, v_shape, v_pitch, P, rx_pitch, dws, roi, shift_cor, center_slice)
     W = pxrt.Width.SINGLE
     with pxrt.Precision(W):
-        '''
-        V = np.ones(v_shape, dtype=W.value)
-        V = V.reshape(-1)
-
         t_start = time.time()
-        P = op.apply(V)
+        if method == 'pinv':
+            stop_crit = pxst.MaxIter(max_iter)
+            V_bw = op.pinv(P.reshape(-1), damp=damp, kwargs_fit=dict(stop_crit=stop_crit))
+            
+        elif method == 'tv':
+            stop_crit = pxst.MaxIter(max_iter)
+            x0= np.zeros(v_shape).ravel()
+            grad = Gradient(arg_shape=v_shape)
+            lambda_= damp
+            huber_norm = L1Norm(grad.shape[0]).moreau_envelope(0.01)  # We smooth the L1 norm to facilitate optimisation
+            tv_prior = lambda_ * huber_norm * grad
+
+            # Loss
+            sigma = 1
+            loss = (1/ (2 * sigma**2)) * SquaredL2Norm(dim=P.size).asloss(P.ravel()) * op
+
+            # Smooth part of the posterior
+            smooth_posterior = loss + tv_prior
+            smooth_posterior.diff_lipschitz = 3e3 #smooth_posterior.estimate_diff_lipschitz()
+
+            # Define the solver
+            solver = PGD(f=smooth_posterior, show_progress=True, verbosity=1)
+
+            # Call fit to trigger the solver
+            stop_crit = pxst.RelError(eps=1e-3, var="x", f=None, norm=2, satisfy_all=True) | pxst.MaxIter(max_iter)
+            solver.fit(x0=x0, acceleration=True, stop_crit=stop_crit)
+            recon_tv = solver.solution().squeeze()
+            V_bw = recon_tv.reshape(v_shape)
+
         t_stop = time.time()
-        print("forward: ", t_stop - t_start)
-        '''
+        #print("backward: ", t_stop - t_start)
+        
+        return V_bw.reshape(v_shape)
 
-        t_start = time.time()
-        #V_bw = op.adjoint(P.reshape(-1))
-        stop_crit = pxst.MaxIter(80)
-        V_bw = op.pinv(P.reshape(-1), damp=8, kwargs_fit=dict(stop_crit=stop_crit))
-        t_stop = time.time()
-        print("backward: ", t_stop - t_start)
+def shift_correction(fpath, dataset, dws, rx_pitch, range, step):
+    '''
+    Parameters
+    ----------
+    fpath: plib.Path
+        Folder where .xml file and raw data is located.
+    dataset: str
+        Name of dataset.
+    dws: int
+        Down-sampling rate.
+    rx_pitch: tuple[float]
+        (x, y) detector pitch.
+    range: tuple[float]
+        Range of shift correction values.
+    step: float
+        Step size of shift correction values.
+    Returns
+    -------
+    shift_corr: float
+        Shift correction value for center of rotation.
+    '''
 
-        plt.figure()
-        plt.imshow(V_bw.reshape(v_shape)[:, v_shape[1]//2, :], cmap='gray')
-        plt.show()          
+    #find correct shift correction with plotting curve of sharpness if reconstructions for different shift corrections
+    shift_corr = np.arange(-range[0], range[1], step)
+    sharpness = np.zeros(shift_corr.shape)
+    for i in range(len(shift_corr)):
+        #sharpness in fourier space : mask to keep only high frequencies and then sum absolute values
+        rec = reconstruct(fpath, dataset, dws, rx_pitch, method = 'pinv', roi='False', shift_cor=shift_corr[i], center_slice='True', max_iter=4, damp=0.05)
+        rec = rec[:, rec.shape[1]//2, :]
+        fourier_image = np.fft.fftshift(np.fft.fft2(rec))
+        fourier_image = np.abs(fourier_image)
+        mask = np.ones(fourier_image.shape)
+        x, y = np.ogrid[:fourier_image.shape[0], :fourier_image.shape[1]]
+        mask = np.sqrt((x - fourier_image.shape[0]//2)**2 + (y - fourier_image.shape[1]//2)**2) <= 150
+        sharpness[i] = np.sum(fourier_image*mask)
 
-        breakpoint()
+    return shift_corr[np.argmax(sharpness)]
+
+if __name__ == "__main__":
+    # All coordinates are in XYZ ordering.
+    # Reference frame centered at base of rotating plate, with:
+    # * XZ-plane horizontal (touching bottom of detector);
+    # * Y-axis pointing up;
+    # * Z-axis pointing towards TX.
+
+    # A-priori knowledge of the datasets and machine. =========================
+    # dataset, dws = "000-HR", 2  # folder, down-sampling rate
+    dataset, dws = "bin2", 2
+    rx_pitch = (127e-3, 127e-3)  # 0.127 [mm] detector/receiver pitch (height, width)
+    
+    # Load setup parameters from disk. ========================================
+    froot = plib.Path("~/Downloads/M2EA99-000").expanduser()
+    fpath = froot / dataset
+
+    shiftcorr_value = -0.293 #precalculated shift correction value
+    #shift_correction(fpath, dataset, dws, rx_pitch, range=[-0.5, 0.1], step=0.01) #comment/uncomment to calculate shift correction value
+    print('shift correction value: ', shiftcorr_value)
+    rec = reconstruct(fpath, dataset, dws, rx_pitch, method = 'pinv', roi='False', shift_cor=shiftcorr_value, center_slice='True', max_iter=100, damp=0.5)
+    
+    plt.figure()
+    plt.imshow(rec[100:500, rec.shape[1]//2, 100:500], cmap='gray', vmax=0.1, vmin=0)
+    plt.show()   
 
 
-    #function doing reconstruction with all this previous code taking as input the output of the function get_info, and also other parameters
-    def setOperator(sod, sdd, v_shape, v_pitch, P, rx_pitch, dws, roi):
-        P = P[:, P.shape[1]//2 - roi: P.shape[1]//2 + roi, :] #ROI taking horizontal slice of the projections
-        N_view, N_h, N_w = P.shape
-        # Compute TX position (1 projection). =====================================
-        rx_h = rx_pitch[0] * dws * N_h  # detector height [mm]
-        rx_w = rx_pitch[1] * dws * N_w  # detector width  [mm]
-        tx_pos = np.r_[0, rx_h / 2, sod]  # (3,)
-
-        # Compute RX position (1 projection). =====================================
-        rx_ll = np.r_[-rx_w / 2, 0, sod - sdd]  # detector lower-left corner
-        Y, X, Z = np.meshgrid(
-            (0.5 * rx_pitch[0] * dws) + np.arange(N_h) * (rx_pitch[0] * dws),  # height (Y-axis)
-            (0.5 * rx_pitch[1] * dws) + np.arange(N_w) * (rx_pitch[1] * dws),  # width  (X-axis)
-            np.r_[0],  # depth (Z-axis)
-            indexing="ij",
-        )
-        rx_pos = rx_ll + np.concatenate([X, Y, Z], axis=-1)  # (N_h, N_w, 3)
-
-        # Compute VOL position. ===================================================
-        v_dim = np.r_[v_shape] * np.r_[v_pitch]  # volume XYZ dimensions [mm]
-        v_center = np.r_[0, rx_h / 2, 0]  # volume center
-        v_ll = v_center - v_dim / 2  # volume lower-left corner.
-
-        # Compute ray parameterizations (1 projection). ===========================
-        n_spec = np.reshape(rx_pos - tx_pos, (-1, 3))  # (N_h * N_w, 3)
-        t_spec = np.broadcast_to(tx_pos, n_spec.shape)  # (N_h * N_w, 3)
-
-        # Expand n/t_spec to include all rotations. ===============================
-        angle = np.linspace(0, 2 * np.pi, num=N_view, endpoint=False)
-        R = np.zeros(
-            (N_view, 3, 3),
-        )
-        R[:, 0, 0] = np.cos(angle)
-        R[:, 2, 2] = np.cos(angle)
-        R[:, 2, 0] = np.sin(angle)
-        R[:, 0, 2] = -np.sin(angle)
-        R[:, 1, 1] = 1
-
-        n_spec = (n_spec @ R.transpose(0, 2, 1)).reshape(-1, 3)  # (N_view * N_h * N_w, 3)
-        t_spec = (t_spec @ R.transpose(0, 2, 1)).reshape(-1, 3)  # (N_view * N_h * N_w, 3)
-
-        op = pxr.XRayTransform.init(
-            arg_shape=v_shape,
-            origin=v_ll,
-            pitch=v_pitch,
-            method="ray-trace",
-            n_spec=n_spec,
-            t_spec=t_spec,
-        )
-        # Compute TX position (1 projection). =====================================
-
-        return op
-
-    #function with only path
+    breakpoint()
